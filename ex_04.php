8f5c95fb4a400b408ed79ec2c6dea525a3b812ed
SamuelCandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-07-08 12:35:50 -0300

    add: ex_04

--- ex_04.php
+++ ex_04.php
@@ -1,8 +1,19 @@
 <?php
 
-function gerarSenha($senha){
+function gerarSenha($tamanho){
+$ma = "ABCDEFGHIJKLMNOPQRSTUVYXWZ";
+$mi = "abcdefghijklmnopqrstuvyxwz";
+$nu = "0123456789";
+$si = "!@#$%¨&*()_+=";
+$senha = "";
 
+$senha .= str_shuffle($ma);
+$senha .= str_shuffle($mi);
+$senha .= str_shuffle($nu);
+$senha .= str_shuffle($si);
 
-
+return substr(str_shuffle($senha),0,$tamanho);
 
 }
+
+echo gerarSenha(64);
